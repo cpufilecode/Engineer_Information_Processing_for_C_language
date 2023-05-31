0e85f0d95f44d2b3d6aca6557f9124fb85270ea1
cpufilecode <zf0908@naver.com>
2023-05-31 23:05:00 +0900

    upload

--- C_language/c_118.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void main()
-{
-	int i, j, k;
-	scanf_s("%d %d", &i, &j);
-	k = i + j;
-	printf("%d\n", k);
-}
--- C_language/c문제5번.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-void main()
-{
-	int i, j;
-	scanf_s("%o#%x", &i, &j);
-	printf("%d %d", i, j);
-}
