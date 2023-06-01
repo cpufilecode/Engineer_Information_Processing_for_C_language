9cd01e25e91781f3863a6813a472937e8f0a0bfb
cpufilecode <zf0908@naver.com>
2023-06-01 23:34:36 +0900

    #

---
+++ 119/c문제11번.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main() {
+	int i, a[5], cnt = 0;
+	for (i = 0; i < 5; i++)
+		scanf_s("%d", &a[i]);
+	for (i = 0; i < 5; i++) {
+		if (a[i] % 2 != 0)
+			cnt = cnt + 1;
+	}
+	printf("홀수의 개수 : %d개", cnt);
+}
