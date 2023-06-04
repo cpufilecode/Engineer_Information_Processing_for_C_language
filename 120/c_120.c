806f4f2396131c1a5adf50d5dd221efca0f4e774
cpufilecode <zf0908@naver.com>
2023-06-04 15:52:32 +0900

    upload

--- 120/c_120.c
+++ 120/c_120.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 void main()
 {
-	//int a = 50;
-	//int *b = &a;
-	//*b = *b + 20;
-	//printf("%d, %d", a, *b);
+	int a = 50;
+	int *b = &a;
+	*b = *b + 20;
+	printf("%d, %d \n", a, *b);
 	char* s;
 	s = "gilbut";
 	for (int i = 0; i < 6; i += 2) {
